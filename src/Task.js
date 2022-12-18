5d70f32a241b736daa3a8e1d4a9b695ba7db390e
Niken Riri <nikenriri05@gmail.com>
2022-12-19 00:07:50 +0700

    Push File to Deploy

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Niken Riri Dwi Astuti" // TODO: replace this
+const studentId = "FE2779038" // TODO: replace this
+
+export { url, studentName, studentId }
